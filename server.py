b77e8823d96f8f3b2671cf13d7aadfde721947ce
Gurgel69 <33703214+Gurgel69@users.noreply.github.com>
2020-07-03 13:49:47 +0200

    Added header with length information.

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
 import socket
 
+HEADERSIZE = 4
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((socket.gethostname(), 1234))
 serversocket.listen()
-
+print(f"Listenning at: {serversocket}")
 print("Waiting for someone to connect...")
 while True:
     clientsocket, address = serversocket.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes(f"The server {socket.gethostname()} salutes you for wanting to communicate ", "utf-8"))
-    clientsocket.send(bytes(socket.gethostname(), "utf-8"))
-    clientsocket.send(bytes(".", "utf-8"))
+    msg = f"The server {socket.gethostname()} salutes you for wanting to communicate."
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    print(f"len(msg) is {len(msg)} msg = '{msg}'")
+    clientsocket.send(bytes(msg, "utf-8"))
+    #clientsocket.send(bytes(f"The server {socket.gethostname()} salutes you for wanting to communicate ", "utf-8"))
+    #msg = f"The server {socket.gethostname()} is closing its socket."
+    #msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    #print(msg)
+    #clientsocket.send(bytes(msg, "utf-8"))
+    #clientsocket.send(bytes(f"The server {socket.gethostname()} is closing its socket.", "utf-8"))
     clientsocket.close()
